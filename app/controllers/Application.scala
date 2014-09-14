a516ef08f4fea1fd3452df06d7305470fcaaade1
Harold Lee <hlee@linkedin.com>
2014-09-14 16:37:05 -0700

    Rate limiting logic, show more fields.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
+import java.util.Date
 
 import play.api._
 import play.api.libs.json.{JsObject, JsArray, JsValue}
@@ -30,19 +31,14 @@ object Application extends Controller {
       query match {
         case None => Future.successful(Redirect(routes.Application.index))
         case Some(q) => {
-          val followingReq = following
           val searchReq1 = searchUsers(q, 1)
           val searchReq2 = searchUsers(q, 2)
           for {
             users1 <- searchReq1
             users2 <- searchReq2
-            followedUsers <- followingReq
           } yield {
-            val users = users1 ++ users2
-            val followedNames = followedUsers.map(_ \ "screen_name").toSet
-            val userNames = users.toSeq map TwitterUser
-
-            Ok(views.html.search(q, userNames))
+            val users = (users1 ++ users2) map TwitterUser
+            Ok(views.html.search(q, users))
           }
         }
       }
@@ -53,12 +49,29 @@ object Application extends Controller {
     // Helpers for extracting Twitter fields from user objects
     def handle = (json \ "screen_name").as[String]
     def userName = (json \ "name").as[String]
+    def description = (json \ "description").as[String]
     def location = (json \ "location").as[String]
-    def followers = (json \ "followers").as[Int]
+    def tweets = (json \ "statuses_count").as[Int]
+    def followed = (json \ "following").as[Boolean]
+    def followers = (json \ "followers_count").as[Int]
+    def following = (json \ "friends_count").as[Int]
+    def followersStr = "%,d".format(followers)
+    def followingStr = "%,d".format(following)
+    def ratioFollowingToFollowers = following.toDouble / followers.toDouble
   }
+
+  // The rate limit ceiling for that given request.
+  val RESPONSE_HEADER_RATE_LIMIT = "X-Rate-Limit-Limit"
+
+  // The number of requests left for the 15 minute window.
+  val RESPONSE_HEADER_RATE_LIMIT_REMAINING = "X-Rate-Limit-Remaining"
+
+  // The remaining window before the rate limit resets in UTC epoch seconds.
+  val RESPONSE_HEADER_RATE_LIMIT_RESET = "X-Rate-Limit-Reset"
 
   def following(implicit request: RequestHeader, token: OAuthCalculator): Future[Seq[JsObject]] = {
     twitterGet("/friends/list.json").map { response =>
+      throwOnRateLimiting(response)
       (response.json \ "users").as[JsArray].value.map { _.as[JsObject] }
     }
   }
@@ -71,8 +84,21 @@ object Application extends Controller {
       throw new IllegalArgumentException("Pagination via the Twitter API requires the count to be between 1 and 20, inclusive.")
     val encodedQuery = URLEncoder.encode(query, "UTF-8")
     twitterGet(s"/users/search.json?q=${encodedQuery}&page=${page}&count=${count}").map { response =>
+      throwOnRateLimiting(response)
       response.json.as[JsArray].value.map(_.as[JsObject])
     }
+  }
+
+  def throwOnRateLimiting(response: WSResponse) {
+    if (response.status == 429) {
+      throw new Exception("Twitter rate limiting!")
+    }
+    val limit: Option[String] = response.header(RESPONSE_HEADER_RATE_LIMIT)
+    val remaining: Option[String] = response.header(RESPONSE_HEADER_RATE_LIMIT_REMAINING)
+    val reset: Option[Date] = response.header(RESPONSE_HEADER_RATE_LIMIT_RESET).map {
+      str => new Date(str.toLong * 1000)
+    }
+    Logger.info(s"Twitter rate limiting stats: limit: $limit remaining: $remaining reset: $reset")
   }
 
   def requestToken()(implicit request: RequestHeader): Option[RequestToken] = {
